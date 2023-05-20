0574333f9328724ba59e7351705f3309885be2f2
abhishek gupta <ak4838960@gmail.com>
2023-05-20 23:21:41 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("hii daeer");
+    System.out.println("Helljo World dear!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+  }
+}
